c7d0fe3ac6ef33bf5b8d6a1e1b547bea8426916b
explosion-mental <explosion0mental@gmail.com>
2022-03-03 23:14:35 -0500

    add a bar, more output in Xevents and some comments

--- config.h
+++ config.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
+static int showbar         = 1;          /* 0 means no bar */
 static const int winwidth  = 800;        /* default window width */
 static const int winheight = 600;        /* default window height */
 static const char *fonts[] = {
@@ -12,13 +13,14 @@ static const char *fonts[] = {
 static const char *colors[][3] = {
       			/*  fg       bg     */
 	[SchemeNorm]  = { "#eeeeee", "#005577" },
-	[SchemeSel]   = { "#bbbbbb", "#222222" },
+	[SchemeBar]   = { "#bbb012", "#411828" },
 };
 
 static Shortcut shortcuts[] = {
 	/* keysym         function        argument */
 	{ XK_Escape,      quit,           {0} },
 	{ XK_q,           quit,           {0} },
+	{ XK_b,           togglebar,      {0} },
 };
 
 static Mousekey mshortcuts[] = {
--- xwindow.c
+++ xwindow.c
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 #include <sys/types.h>
+#include <sys/time.h>
 #include <arpa/inet.h>
 
 #include <errno.h>
@@ -11,6 +12,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <X11/keysym.h>
 #include <X11/XKBlib.h>
@@ -29,7 +31,7 @@ char *argv0;
 #define LENGTH(a)         (sizeof(a) / sizeof(a)[0])
 #define TEXTW(X)          (drw_fontset_getwidth(drw, (X)) + lrpad)
 
-enum { SchemeNorm, SchemeSel }; /* color schemes */
+enum { SchemeNorm, SchemeBar }; /* color schemes */
 enum { WMDelete, WMName, WMLast }; /* atoms */
 
 /* Purely graphic info */
@@ -65,8 +67,10 @@ static void run(void);
 static void usage(void);
 static void xhints(void);
 static void setup(void);
+static void drawbar(void);
 
 static void quit(const Arg *arg);
+static void togglebar(const Arg *arg);
 
 /* X events */
 static void bpress(XEvent *);
@@ -77,11 +81,12 @@ static void configure(XEvent *);
 
 /* variables */
 static Atom atoms[WMLast];
+static char right[128], left[128];
 static XWindow xw;
 static Drw *drw;
 static Clr **scheme;
 static int running = 1;
-//static int bh, blw = 0;      /* bar geometry */
+static int bh = 0;      /* bar geometry */
 static int lrpad;            /* sum of left and right padding for text */
 
 /* config.h for applying patches and the configuration. */
@@ -113,6 +118,58 @@ void
 quit(const Arg *arg)
 {
 	running = 0;
+}
+
+void
+togglebar(const Arg *arg)
+{
+	showbar = !showbar;
+	if (showbar) {
+		xw.h -= bh;
+		printf("BAR IS ON\n");
+	} else {
+		xw.h += bh;
+		printf("bar is off\n");
+	}
+	XClearWindow(xw.dpy, xw.win);
+	XSync(xw.dpy, False);
+	drawbar();
+}
+
+void
+drawbar(void)
+{
+	int y, tw = 0;
+
+	if (!showbar)
+		return;
+
+	/* currently topbar is not supported */
+	y = xw.h - bh;
+
+	drw_setscheme(drw, scheme[SchemeBar]);
+
+	/* left text */
+	struct timeval tv;
+	time_t t;
+	struct tm *info;
+	char buffer[64];
+
+	gettimeofday(&tv, NULL);
+	t = tv.tv_sec;
+	info = localtime(&t);
+	strftime(buffer, sizeof(buffer), "Today is %A, %B %d..", info);
+
+	snprintf(left, LENGTH(left), "%s", buffer);
+	drw_text(drw, 0, y, xw.w / 2, bh, lrpad / 2, left, 0);
+
+	/* right text */
+	char str[] = "..And is a good day!";
+	snprintf(right, LENGTH(right), "%s", str);
+	tw = TEXTW(right) - lrpad + 2; /* 2px right padding */
+	drw_text(drw, xw.w/2, y, xw.w/2, bh, xw.w/2 - (tw + lrpad / 2), right, 0);
+
+	drw_map(drw, xw.win, 0, y, xw.w, bh);
 }
 
 void
@@ -157,6 +214,7 @@ setup(void)
 	if (!(xw.dpy = XOpenDisplay(NULL)))
 		die("xwindow: Unable to open display");
 
+	/* init screen */
 	xw.scr = XDefaultScreen(xw.dpy);
 	xw.vis = XDefaultVisual(xw.dpy, xw.scr);
 
@@ -169,11 +227,13 @@ setup(void)
 	attrs.event_mask = KeyPressMask | ExposureMask | StructureNotifyMask |
 	                      ButtonMotionMask | ButtonPressMask;
 
+	/* init window */
 	xw.win = XCreateWindow(xw.dpy, XRootWindow(xw.dpy, xw.scr), 0, 0,
 	                       xw.w, xw.h, 0, XDefaultDepth(xw.dpy, xw.scr),
 	                       InputOutput, xw.vis, CWBitGravity | CWEventMask,
 	                       &attrs);
 
+	/* init atoms */
 	atoms[WMDelete] = XInternAtom(xw.dpy, "WM_DELETE_WINDOW", False);
 	atoms[WMName]   = XInternAtom(xw.dpy, "_NET_WM_NAME", False);
 	XSetWMProtocols(xw.dpy, xw.win, &atoms[WMDelete], 1);
@@ -181,16 +241,21 @@ setup(void)
 	if (!(drw = drw_create(xw.dpy, xw.scr, xw.win, xw.w, xw.h)))
 		die("xwindow: Unable to create drawing context");
 
-	/* init scheme */
+	/* init appearance */
 	scheme = ecalloc(LENGTH(colors), sizeof(Clr *));
 	for (i = 0; i < LENGTH(colors); i++)
 		scheme[i] = drw_scm_create(drw, colors[i], 2);
 
 	XSetWindowBackground(xw.dpy, xw.win, scheme[SchemeNorm][ColBg].pixel);
 
+	/* init fonts */
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
+	bh = drw->fonts->h + 2; /* two pixel padding */
+
+	/* init bar */
+	drawbar();
 
 	XStringListToTextProperty(&argv0, 1, &prop);
 	XSetWMName(xw.dpy, xw.win, &prop);
@@ -205,6 +270,7 @@ bpress(XEvent *e)
 {
 	unsigned int i;
 
+	printf("\tmouse 'Button%d'\n", e->xbutton.button);
 	for (i = 0; i < LENGTH(mshortcuts); i++)
 		if (e->xbutton.button == mshortcuts[i].b && mshortcuts[i].func)
 			mshortcuts[i].func(&(mshortcuts[i].arg));
@@ -213,15 +279,22 @@ bpress(XEvent *e)
 void
 cmessage(XEvent *e)
 {
-	if (e->xclient.data.l[0] == atoms[WMDelete])
+	printf("XEVENT ClientMessage\n");
+	if (e->xclient.data.l[0] == atoms[WMDelete]) {
+		printf("Handling ClientMessage, closing window..\n");
 		running = 0;
+	}
 }
 
 void
 expose(XEvent *e)
 {
-	if (0 == e->xexpose.count)
-		printf("expose");
+	printf("XEVENT Expose\n");
+	if (0 == e->xexpose.count) {
+		printf("Handling expose '%d'\n", e->xexpose.count);
+		XClearWindow(xw.dpy, xw.win);
+		drawbar();
+	}
 }
 
 void
@@ -231,6 +304,7 @@ kpress(XEvent *e)
 	KeySym sym;
 
 	sym = XkbKeycodeToKeysym(xw.dpy, (KeyCode)e->xkey.keycode, 0, 0);
+	printf("\tkeypress '%s'\n", XKeysymToString(sym));
 	for (i = 0; i < LENGTH(shortcuts); i++)
 		if (sym == shortcuts[i].keysym && shortcuts[i].func)
 			shortcuts[i].func(&(shortcuts[i].arg));
@@ -246,7 +320,7 @@ configure(XEvent *e)
 		printf("Handling configurenotify (width: '%d', height: '%d')\n", ev->width, ev->height);
 		xw.w = ev->width;
 		xw.h = ev->height;
-		drw_resize(drw, xw.w, xw.h);
+		drw_resize(drw, xw.w, xw.h + bh);
 	}
 }
 
